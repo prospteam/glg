f60db59cfa5a6754497aa137e424c23bf7e45df1
jovylle <jbermudez@proweaver.net>
2019-07-31 14:35:40 +0800

    Jov MAPS 7/31

--- App/BWBSafeRide/android/app/src/main/java/com/bwbsaferide/MainApplication.java
+++ App/BWBSafeRide/android/app/src/main/java/com/bwbsaferide/MainApplication.java
@@ -3,6 +3,7 @@ package com.bwbsaferide;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new VectorIconsPackage(),
             new RNSpinkitPackage(),
             new AsyncStoragePackage(),
--- App/BWBSafeRide/src/components/Dashboard.js
+++ App/BWBSafeRide/src/components/Dashboard.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
-import { Alert } from 'react-native';
+import { Dimensions, Alert } from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 import AsyncStorage from '@react-native-community/async-storage';
+import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.771707;
+const LONGITUDE = -122.4053769;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const origin = {latitude: 37.3318456, longitude: -122.0296002};
+const destination = {latitude: 37.771707, longitude: -122.4053769};
+const GOOGLE_MAPS_APIKEY = 'AIzaSyC8lpkvXFDua9S2al669zfwz7GSkeVFWs4';
+
+
+const getCurrentLocation = () => {
+  alert('1');
+}
 
 export default class Dashboard extends Component {
     static navigationOptions = {
@@ -9,18 +27,17 @@ export default class Dashboard extends Component {
       drawerIcon: () => (
           <Icon type="FontAwesome" name="home" style={{ fontSize: 22 }} />
       )
-
     };
 
     checkSession = async () => {
       console.log('here');
-      if(await AsyncStorage.getItem('userData')){
+      if(await AsyncStorage.getItem('userData')) {
         // this.setState({
         //   isLogged: true,
         //   });
         console.log('naa');
         console.log(await AsyncStorage.getItem('userData'));
-      }else{
+      }else {
           this.props.navigation.navigate('Logout');
       }
       // setTimeout(() => {
@@ -31,6 +48,9 @@ export default class Dashboard extends Component {
     }
 
     render() {
+
+      console.log(Dimensions.get('window'));
+
       console.log("calling");
       this.checkSession();
 
@@ -44,13 +64,22 @@ export default class Dashboard extends Component {
              </Right>
             </Header>
                 <Content>
-                  <Text>
-                    Dashboard/Home Screen content goes here ...
-                  </Text>
+                  <MapView style={{flex: 1,  height: height,  width: width}} initialRegion={{
+                      latitude: LATITUDE,
+                      longitude: LONGITUDE,
+                      latitudeDelta: LATITUDE_DELTA,
+                      longitudeDelta: LONGITUDE_DELTA,
+                    }}>
+                    <MapViewDirections
+                      origin={origin}
+                      destination={destination}
+                      apikey={GOOGLE_MAPS_APIKEY}
+                    />
+                  </MapView>
                 </Content>
                 <Footer>
                     <FooterTab style={{backgroundColor:"#1c1b22"}}>
-                        <Button vertical active>
+                        <Button vertical active onPress={() => getCurrentLocation()}>
                             <Icon active name="map" />
                             <Text>Book Now</Text>
                         </Button>
