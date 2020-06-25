95d28441bf455d9f606f1c29d76bc1c2cf8b413b
Rogen <rmpagsanjan@proweaver.net>
2020-06-25 17:55:35 +0800

    Jovylle <3 Jorecylle = BAGAY na BAGAY

--- GLGTrackerSystem/GLGTrackingSystem/App.js
+++ GLGTrackerSystem/GLGTrackingSystem/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {ScrollView, TouchableOpacity, TextInput, ImageBackground, Image } from 'react-native';
 import { Text, Form, Item, Input, Label, Icon, Button, Card, CardItem, Body, View  } from 'native-base';
-import Dash from 'react-native-dash';
 
 
 import truck_bg from './src/assets/images/truck_bg.jpg';
@@ -10,22 +9,53 @@ import bg_image from './src/assets/images/bg_image.png';
 import styles from './src/assets/styles/Commonstyles.js';
 
 
-export default function App() { 
+export default function App() {
   return (
       <View>
           <ImageBackground source={bg_image} style={{ width: '100%', height: '100%', marginBottom: 65, blurRadius: 5}}>
-              <View style={{ marginTop: "30%", justifyContent: "center", alignItems: "center", marginTop: 25}}>
-                  <Image source={logo} style={{ width: 150, height: 150, borderRadius: 100, blurRadius: 5 }} />
+              <View style={{justifyContent: "center", alignItems: "center"}}>
+                  <Image source={truck_bg} style={{ width: '100%', height: 180, }} />
+              </View>
+              <View style={{justifyContent: "center", alignItems: "center"}}>
+                  <Image source={logo} style={{ width: 150, height: 150, borderRadius: 100, marginTop: -65,  }} />
               </View>
               <View  style={{justifyContent: "center", alignItems: "center", marginTop: 15}}>
                 <Text style={{color:'white', fontSize:25, fontFamily: 'sans-serif' }}>Track Numbers: </Text>
                         <TextInput style={styles.trackinput} placeholder="0123456789"  placeholderTextColor="white"/>
               </View>
-              <View style={{flexDirection: 'column', marginLeft:0}}>
-                  <Dash dashColor={'#57B9BB'} style={styles.dash}/>
-                  <Icon type="FontAwesome5" name='truck' style={{color:'orange', fontSize:15, marginLeft:20}}/>
-                  <Dash dashColor={'#57B9BB'} style={styles.dash}/>
-              </View>
+              <View style={{flexDirection: 'column', justifyContent: "center", alignItems: "center"}}>
+
+                      <View style={{ margin: 10}} />
+
+                      <View style={{flexDirection: 'row'}}>
+                          <Icon name='ios-checkmark-circle' style={{color:'#24cea7', fontSize:20,}}/>
+                          <View style={{flexDirection: 'column'}}>
+                            <Text style={{fontSize:15, color:'white', marginLeft:30}}>Mon, 01 June 2020</Text>
+                            <Text style={{fontSize:18, color:'white', marginLeft:30, fontWeight: 'bold'}}>Claimed </Text>
+                          </View>
+                      </View>
+
+                      <View style={{ margin: 10}} />
+
+                      <View style={{flexDirection: 'row'}}>
+                          <Icon name='ios-checkmark-circle' style={{color:'#24cea7', fontSize:20,}}/>
+                          <View style={{flexDirection: 'column'}}>
+                            <Text style={{fontSize:15, color:'white', marginLeft:30}}>Mon, 01 June 2020</Text>
+                            <Text style={{fontSize:18, color:'white', marginLeft:30, fontWeight: 'bold'}}>Arrived at USA</Text>
+                          </View>
+                      </View>
+
+                      <View style={{ margin: 10}} />
+
+                      <View style={{flexDirection: 'row'}}>
+                          <Icon name='ios-checkmark-circle' style={{color:'#24cea7', fontSize:20,}}/>
+                          <View style={{flexDirection: 'column'}}>
+                            <Text style={{fontSize:15, color:'white', marginLeft:30}}>Mon, 01 June 2020</Text>
+                            <Text style={{fontSize:18, color:'white', marginLeft:30, fontWeight: 'bold'}}>Delivered </Text>
+                          </View>
+                      </View>
+
+                </View>
           </ImageBackground>
       </View>
 
