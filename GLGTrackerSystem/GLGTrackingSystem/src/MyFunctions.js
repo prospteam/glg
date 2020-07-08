c8ae66e1d905c2ba18c6592f1a4c7f91fd261c53
twero <twero001@gmail.com>
2020-07-08 09:50:27 +0800

    jovjov 7/8/2020 thisAppIsTheBest

---
+++ GLGTrackerSystem/GLGTrackingSystem/src/MyFunctions.js
@@ -0,0 +1,23 @@
+// import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+
+// Dont just exist, live.
+// class MyFunctions extends Component {
+class MyFunctions {
+	constructor(props){
+        super(props);
+        // my_function();
+    }
+    
+    my_function = () => {
+        return 1;
+    }
+}
+// KUNG GUSTO MONG GAMITIN ANG REDUX FUNCTIONS(YUNG NASA ACTIONS)
+function reduxActionFunctions(dispatch){
+    return bindActionCreators({
+		// si set_sampleString function kay makit an sa actions folder
+    },dispatch);
+ }
+export default connect(null,reduxActionFunctions)(MyFunctions);
---
+++ GLGTrackerSystem/GLGTrackingSystem/src/assets/styles/MyStyles.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    background_red:{
+        backgroundColor: 'red',
+    },
+});
