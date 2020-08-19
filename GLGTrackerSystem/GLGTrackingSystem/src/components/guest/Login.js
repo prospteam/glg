797cb7bec56227bf4c2659f8339c033c3920270e
jovylle bermudez taga lapu lapu city <jovyllebermudez@gmail.com>
2020-08-19 14:03:40 +0800

    jov followup update 8-19

--- GLGTrackerSystem/GLGTrackingSystem/src/components/guest/Login.js
+++ GLGTrackerSystem/GLGTrackingSystem/src/components/guest/Login.js
@@ -151,7 +151,7 @@ class Login extends Component {
                         <TextInput style={styles.text_input} placeholder="Password"  placeholderTextColor="white" onChangeText={text => this.setState({ password: text })}/>
                     </View>
                     <View  style={{justifyContent: "center", alignItems: "center", marginTop: 30}}>
-                        <TouchableOpacity style={styles.button} onPress={() => Actions.Carrier()}>
+                        <TouchableOpacity style={styles.button} onPress={() => this.form_login()}>
                             <Text style={{fontSize:18}}>Log In</Text>
                         </TouchableOpacity>
                     </View>
